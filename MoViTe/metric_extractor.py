f7cf42e2d4d33c9f4f136cc84d5a8203b507ee4f
trunghieu1109 <101007868+trunghieu1109@users.noreply.github.com>
2024-08-20 16:22:53 +0700

    fix ttc calculating in metric_extractor

--- MoViTe/metric_extractor.py
+++ MoViTe/metric_extractor.py
@@ -33,13 +33,20 @@ def metrics_extract(exp_file):
     
     collision_by_random_action = 0
     collision_by_model_choosing_action = 0
+    sudden_appearance_ = 0
+    repeated_collision_ = 0
+    overlapping_ = 0
     
     for index, row in df.iterrows():
         
+        eps = row['Episode']
         reward = float(row['Collision_Probability'])
         done = row['Done']
         action = row['Action']
         cp_list = row['Collision_Probability_Per_Step']
+        sudden_appearance = row['Sudden Appearance']
+        overlapping = row['Overlapping']
+        repeated_collision = row['Repeated Collision']
         state = row['State'][1:-1].split(",")[0].split(" ")
         state_ = [s for s in state if s != ""]
         uid = row["Collision_uid"]
@@ -52,51 +59,87 @@ def metrics_extract(exp_file):
         if reward > 0: 
             if reward == 1.0:
                 
-                if prev_uid == uid:
-                    new_pos = np.array([float(state_[0]), float(state_[1]), float(state_[2])])
-                    diff = np.linalg.norm(prev_position - new_pos)
-                    
-                    if diff > 2:
+                # if prev_uid == uid:
+                #     new_pos = np.array([float(state_[0]), float(state_[1]), float(state_[2])])
+                #     diff = np.linalg.norm(prev_position - new_pos)
+                       
+                #     if diff > 2:
+                #         distinct_collision += 1
+                           
+                # else:
+                #     distinct_collision += 1
+                       
+                # prev_position = np.array([float(state_[0]), float(state_[1]), float(state_[2])])
+                # prev_uid = uid
+                   
+                # collision += 1
+            
+                if not overlapping and not sudden_appearance and not repeated_collision:
+                    if prev_uid == uid:
+                        new_pos = np.array([float(state_[0]), float(state_[1]), float(state_[2])])
+                        diff = np.linalg.norm(prev_position - new_pos)
+                        
+                        if diff > 2:
+                            distinct_collision += 1
+                            
+                    else:
                         distinct_collision += 1
                         
-                else:
-                    distinct_collision += 1
+                    prev_position = np.array([float(state_[0]), float(state_[1]), float(state_[2])])
+                    prev_uid = uid
                     
-                prev_position = np.array([float(state_[0]), float(state_[1]), float(state_[2])])
-                prev_uid = uid
-                
-                collision += 1
-                
-                # if type == "by model":
-                #     collision_by_model_choosing_action += 1
-                # else:
-                #     collision_by_random_action += 1
-                
-                if not isCollision:
-                    max_val = 0.0
-                    index = 0
-                    cnt = 0
+                    collision += 1
                     
-                    for cp in cp_list[1:-1].split(","):
-                        fcp = float(cp)
-                        if fcp > max_val:
-                            max_val = fcp
-                            index = cnt
-                            
-                        cnt += 1
+                    # print("Eps: ", eps)
+                    
+                    if type == "by model":
+                        collision_by_model_choosing_action += 1
+                    else:
+                        collision_by_random_action += 1
+                    
+                    if not isCollision:
+                        max_val = 0.0
+                        index = 0
+                        cnt = 0
                         
-                    time_to_collision += 0.5 * (index + 1)
-                
-                isCollision = True
+                        for cp in cp_list[1:-1].split(","):
+                            fcp = float(cp)
+                            if fcp > max_val:
+                                max_val = fcp
+                                index = cnt
+                                
+                            cnt += 1
+                            
+                        time_to_collision += 0.5 * (index + 1)
+                    
+                    isCollision = True
+                else:
+                    # print("sudden: ", sudden_appearance)
+                    if sudden_appearance:
+                        sudden_appearance_ += 1
+                    # print("repeated: ", repeated_collision)
+                    if repeated_collision:
+                        repeated_collision_ += 1
+                    # print("over: ", overlapping)
+                    if overlapping:
+                        overlapping_ += 1
             else:
-                potential_collision += 1
-                time_to_collision += obs_time
+                if reward > 0.5:
+                    potential_collision += 1
+                    
+                if not isCollision:
+                    time_to_collision += obs_time
         else:
-            time_to_collision += obs_time
+            if not isCollision:
+                time_to_collision += obs_time
                 
         if done:
             cnt_eps_collision += int(isCollision)
             avg_ttc += time_to_collision * int(isCollision)
+            
+            if isCollision:
+                print("Time to Collision: ", time_to_collision, "Episode: ", eps)
+            
             isCollision = False
             isPotential = False
             time_to_collision = 0
@@ -108,6 +151,9 @@ def metrics_extract(exp_file):
         reward_info.append(reward)
                 
     print("Collision: ", collision, "\nPotential Collision: ", potential_collision, "\nDistinct Collision: ", distinct_collision, "\nAverage time to collision: ", avg_ttc / max(cnt_eps_collision, 1))
+    print("Sudden Appearance: ", sudden_appearance_)
+    print("Overlapping: ", overlapping_)
+    print("Repeated Collision: ", repeated_collision_)
     print("Collision by model: ", collision_by_model_choosing_action)
     print("Collision randomly: ", collision_by_random_action)
     
