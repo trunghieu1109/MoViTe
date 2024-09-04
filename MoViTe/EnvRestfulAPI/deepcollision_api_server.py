bf39ef10fc23e7973d99f2bac7a175e13549b072
trunghieu1109 <101007868+trunghieu1109@users.noreply.github.com>
2024-09-04 09:50:19 +0700

    add start and end point to deep api server

--- MoViTe/EnvRestfulAPI/deepcollision_api_server.py
+++ MoViTe/EnvRestfulAPI/deepcollision_api_server.py
@@ -600,10 +600,29 @@ def load_scene():
             state.transform.rotation.y = 49
             state.transform.rotation.x = 360
         elif scene == '12da60a7-2fc9-474d-a62a-5cc08cb97fe8':
-            state.transform.position.x = -768.9
+            state.transform.position.x = -328.1
             state.transform.position.y = 10.2
-            state.transform.position.z = 224.1
-            state.transform.rotation.y = 81
+            state.transform.position.z = 45.5
+            state.transform.rotation.y = 314
+            state.transform.rotation.x = 0
+        else:
+            state.transform.position.x = 352.4
+            state.transform.position.y = -7.6
+            state.transform.position.z = -22.6
+            state.transform.rotation.y = 195
+            state.transform.rotation.x = 360
+    elif road_num == '2':
+        if scene == 'bd77ac3b-fbc3-41c3-a806-25915c777022':
+            state.transform.position.x = -76.0
+            state.transform.position.y = 34.3
+            state.transform.position.z = 325.3
+            state.transform.rotation.y = 155
+            state.transform.rotation.x = 359
+        elif scene == '12da60a7-2fc9-474d-a62a-5cc08cb97fe8':
+            state.transform.position.x = -62.7
+            state.transform.position.y = 10.2
+            state.transform.position.z = -110.2
+            state.transform.rotation.y = 224
             state.transform.rotation.x = 0
         else:
             state.transform.position.x = -40.3
@@ -611,13 +630,7 @@ def load_scene():
             state.transform.position.z = -11.8
             state.transform.rotation.y = 105
             state.transform.rotation.x = 1
-    elif road_num == '2':
-        state.transform.position.x = -328.1
-        state.transform.position.y = 10.2
-        state.transform.position.z = 45.5
-        state.transform.rotation.y = 314
-        state.transform.rotation.x = 0
-    elif road_num == '3':
+    else:
         state.transform.position.x = -62.7
         state.transform.position.y = 10.2
         state.transform.position.z = -110.2
@@ -651,17 +664,24 @@ def load_scene():
 
         elif scene == '12da60a7-2fc9-474d-a62a-5cc08cb97fe8':
             requests.post(
-                "http://localhost:8933/LGSVL/SetDestination?des_x=-494.3&des_y=10.2&des_z=294.7")
+                "http://localhost:8933/LGSVL/SetDestination?des_x=-445.7&des_y=10.2&des_z=-22.7")
+        else:
+            requests.post(
+                "http://localhost:8933/LGSVL/SetDestination?des_x=-16.6&des_y=-1.9&des_z=-49.3")
+    elif road_num == '2':
+        if scene == 'bd77ac3b-fbc3-41c3-a806-25915c777022':
+            requests.post(
+                "http://localhost:8933/LGSVL/SetDestination?des_x=93.8&des_y=34.5&des_z=382.2")
+
+        elif scene == '12da60a7-2fc9-474d-a62a-5cc08cb97fe8':
+            requests.post(
+                "http://localhost:8933/LGSVL/SetDestination?des_x=-208.2&des_y=10.2&des_z=-181.6")
         else:
             requests.post(
                 "http://localhost:8933/LGSVL/SetDestination?des_x=348.2&des_y=-7.5&des_z=-64.4")
-    elif road_num == '2':
-        requests.post(
-            "http://localhost:8933/LGSVL/SetDestination?des_x=-445.7&des_y=10.2&des_z=-22.7")
-    elif road_num == '3':
+    else:
         requests.post(
             "http://localhost:8933/LGSVL/SetDestination?des_x=-208.2&des_y=10.2&des_z=-181.6")
-    
     print(road_num)
 
     # ss_route.send(json.dumps(data).encode('utf-8'))
